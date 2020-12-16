af446c603b8baf2a1ea08af695f66366287b1c98
Hinako-Tanaka <youngbird1228@gmail.com>
2020-12-16 22:51:02 +0900

    second commit

--- index.php
+++ index.php
@@ -1,23 +1,38 @@
 <?php
 
     class Human {
-        public $name;
+        private $name;
 
+        // $this->nameのゲッター
         function greeting($greeting) {
             echo $this->name . 'の' . $greeting;
         }
 
-        function setName($namae){
+        // セッター
+        // function setName($namae){
+        //     $this->name = $namae;
+        // }
+
+        // function getName(){
+        //     return $this->name;  //return呼び出し元に返す
+        // }
+
+        // コンストラクタ（セッターの役割もしている）
+        function __construct($namae){
             $this->name = $namae;
         }
 
     }
 
-    $human = new Human();
-    $human->setName('比奈子');
+    $human = new Human('比奈子');
+    // $human->name = 'ヒナコ';
+    // $human->setName('比奈子');
+    //echo $human->getName() . 'です';
     $human->greeting('こんにちは');
-    $human1 = new Human();
-    $human1->setName('ひなこ');
+
+
+    $human1 = new Human('ひなこ');
+    // $human1->setName('ひなこ');
     $human1->greeting('こんばんは');
     // $human->greeting('おはようございます');
     // $human->greeting('こんばんは');
